a7543270198a77f2aeaac9fb52d382b8c20ebcc6
kxx <1dufotze@gmail.com>
2022-12-30 23:23:02 +0100

    added serverside buy part

--- lua/gware-vendors/sv_vendors.lua
+++ lua/gware-vendors/sv_vendors.lua
@@ -1,1 +1,18 @@
---
+net.Receive("gWare.Vendors.BuyItem", function(len, ply)
+    local vendorIndex = net.ReadUInt(7)
+    local itemIndex = net.ReadUInt(7)
+
+    local item = GWARE_VENDORS[vendorIndex].items[itemIndex]
+
+    if not item then return end
+
+    if not ply:canAfford(item.price) then
+        VoidLib.Notify(ply, "Händler", "Du hast nicht genug Geld um diesen Gegenstand zu kaufen!", VoidUI.Colors.Red, 5)
+        return
+    end
+
+    ply:addMoney(-item.price)
+    ply:Give(item.class)
+
+    VoidLib.Notify(ply, "Händler", "Du hast erfolgreich " .. item.name .. " gekauft!", VoidUI.Colors.Green, 5)
+end)
