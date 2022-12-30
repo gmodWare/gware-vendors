430ac156085181e4a29dd42fab62e7899f52c175
kxx <1dufotze@gmail.com>
2022-12-30 18:35:11 +0100

    added vendor class, updated init

--- lua/autorun/gware_vendors-init.lua
+++ lua/autorun/gware_vendors-init.lua
@@ -3,11 +3,13 @@ gWare.Vendors = gWare.Vendors or {}
 
 include("gware-vendors/sh_vendor-class.lua")
 
-
 if SERVER then
     include("gware-vendors/sv_vendor.lua")
+
+    AddCSLuaFile("gware-vendors/sh_vendor-class.lua")
+    AddCSLuaFile("gware-vendors/vgui/cl_vendor.lua")
 end
 
 if CLIENT then
-    
+    include("gware-vendors/vgui/cl_vendor.lua")
 end
--- lua/gware-vendors/sh_vendor-class.lua
+++ lua/gware-vendors/sh_vendor-class.lua
@@ -1,1 +1,31 @@
---
+local VENDOR_CLASS = {}
+VENDOR_CLASS.__index = VENDOR_CLASS
+
+function VENDOR_CLASS:Create(name)
+    local newObject = setmetatable({}, VENDOR_CLASS)
+    newObject.name = name
+    newObject.items = {}
+
+    return newObject
+end
+
+function VENDOR_CLASS:GetItems()
+    return self.items
+end
+
+/* 
+    dataTbl:
+    - item_name: string
+    - item_price: int
+    - item_model: string
+    - item_class: string
+*/
+function VENDOR_CLASS:AddItem(dataTbl)
+    table.Add(dataTbl, self.items)
+end
+
+// public constructor
+function gWare.Vendors.CreateVendor(name)
+    return VENDOR_CLASS:Create(name)
+end
+
