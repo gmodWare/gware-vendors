a2eb5d92d901a8f916ddfc4f7092e45cca2c484f
kxx <1dufotze@gmail.com>
2022-12-31 02:26:51 +0100

    added model to vendor class

--- lua/gware-vendors/sh_vendor-class.lua
+++ lua/gware-vendors/sh_vendor-class.lua
@@ -3,11 +3,12 @@ GWARE_VENDORS = {}
 local VENDOR_CLASS = {}
 VENDOR_CLASS.__index = VENDOR_CLASS
 
-function VENDOR_CLASS:Create(name)
+function VENDOR_CLASS:Create(name, model)
     local newObject = setmetatable({}, VENDOR_CLASS)
     newObject.name = name
     newObject.items = {}
     newObject.id = #GWARE_VENDORS + 1
+    newObject.model = model
 
     table.insert(GWARE_VENDORS, newObject)
 
@@ -42,4 +43,14 @@ end
 // public constructor
 function gWare.Vendors.CreateVendor(name)
     return VENDOR_CLASS:Create(name)
+end
+
+function gWare.Vendors.GetVendor(str)
+    for _, vendor in ipairs(GWARE_VENDORS) do
+        if vendor:GetName() != str then continue end
+
+        return vendor
+    end
+
+    return false
 end
